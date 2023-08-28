d26daec81f43ce305c1b459d08bffb27953dd49b
unknown <ivan.ruskevych@gmail.com>
2023-08-28 21:17:56 +0300

    google auth

--- frontend/src/redux/auth/auth-selectors.js
+++
@@ -1,3 +0,0 @@
-export const getAuthError = ({auth}) => auth.error || {};
-export const isAuth = ({auth}) => auth.isLogin;
-export const getUser = ({auth}) => auth.user;
--- frontend/src/redux/filter/filter-reducer.js
+++
@@ -1,9 +0,0 @@
-import { createReducer } from "@reduxjs/toolkit";
-
-import { setFilter } from "./filter-actions";
-
-const filterReducer = createReducer("", {
-    [setFilter]: (_, {payload}) => payload,
-});
-
-export default filterReducer;
