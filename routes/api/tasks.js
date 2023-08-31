e755b9ce689e28e649b656f99002c776f63582ff
Ivan <ivan.ruskevych@gmail.com>
2023-08-31 13:53:23 +0300

    Update tasks.js

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -3,14 +3,24 @@ const router = express.Router();
 const ctrl = require("../../controllers/controllersTask");
 const { schemas } = require("../../models/task");
 
-const { validateBody, isValidId, authenticate } = require("../../middlewares");
+const { validateBody, isValidId,
+       // authenticate 
+      } = require("../../middlewares");
 
-router.get("/", authenticate, ctrl.getAll);
+router.get("/", 
+           // authenticate, 
+           ctrl.getAll);
 
-router.post("/", authenticate, validateBody(schemas.addTaskSchema), ctrl.add);
+router.post("/", 
+            // authenticate, 
+            validateBody(schemas.addTaskSchema), ctrl.add);
 
-router.delete("/:id", authenticate, isValidId, ctrl.deleteTask);
+router.delete("/:id",
+              // authenticate, 
+              isValidId, ctrl.deleteTask);
 
-router.patch("/:id", authenticate, isValidId, ctrl.update);
+router.patch("/:id",
+             // authenticate, 
+             isValidId, ctrl.update);
 
 module.exports = router;
