761ba149f246bad26c208d887b67d4c2fd1b567e
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-05 13:14:30 +0200

    comments in English, removed extra code

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,4 +1,4 @@
-// ПОЛУЧАЕМ ОТЗЫВЫ С БЕКЭНДА И ОТПРАВЛЯЕМ НА ФРОНТЕНД.
+// GET REVIEWS FROM BACKEND AND SEND TO FRONTEND.
 const express = require('express');
 
 const reviewController = require('../../controllers/controllersReviews');
@@ -8,19 +8,17 @@ const { validateBody, authenticate, isValidId } = require('../../middlewares');
 
 const router = express.Router();
 
-// 1) получить отзывы всех пользователей GET /reviews
+// 1) Get reviews of all users. GET /reviews
 router.get('/', reviewController.getAllReviews);
 
-// 2) получить отзыв пользователя GET /reviews/own
+// 2) Get a user's review. GET /reviews/own
 // router.get('/:id', isValidId, authenticate, reviewController.getUserReview)
 router.get('/own', authenticate, reviewController.getUserReview);
 
-// 3 ) Добавление отзыва.  POST /reviews/own
-
+// 3) Add a review. POST /reviews/own
 router.post('/', authenticate, validateBody(schemas.addReviewSchema), reviewController.addReview);
 
-//  4) Редактирование своего отзыва пользователем PATCH /reviews/own
-
+// 4) Update a user's review. PATCH /reviews/own
 router.patch(
   '/:id',
   isValidId,
@@ -29,8 +27,7 @@ router.patch(
   reviewController.updateReviewById
 );
 
-//  5) Удаление отзыва пользователем
-
+// 5) Delete a user's review.
 router.delete('/:id', isValidId, authenticate, reviewController.deleteReviewById);
 
 module.exports = router;
