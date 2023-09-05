b6df0e89b6636cb79ee7f7aa0dad075ac1132c80
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-05 16:30:50 +0200

    cleared reviews

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,22 +1,27 @@
 // GET REVIEWS FROM BACKEND AND SEND TO FRONTEND.
-const express = require('express');
+const express = require('express')
 
-const reviewController = require('../../controllers/controllersReviews');
+const reviewController = require('../../controllers/controllersReviews')
 
-const { schemas } = require('../../models/review');
-const { validateBody, authenticate, isValidId } = require('../../middlewares');
+const { schemas } = require('../../models/review')
+const { validateBody, authenticate, isValidId } = require('../../middlewares')
 
-const router = express.Router();
+const router = express.Router()
 
 // 1) Get reviews of all users. GET /reviews
-router.get('/', reviewController.getAllReviews);
+router.get('/', reviewController.getAllReviews)
 
 // 2) Get a user's review. GET /reviews/own
-// router.get('/:id', isValidId, authenticate, reviewController.getUserReview)
-router.get('/own', authenticate, reviewController.getUserReview);
+
+router.get('/own', authenticate, reviewController.getUserReview)
 
 // 3) Add a review. POST /reviews/own
-router.post('/', authenticate, validateBody(schemas.addReviewSchema), reviewController.addReview);
+router.post(
+  '/',
+  authenticate,
+  validateBody(schemas.addReviewSchema),
+  reviewController.addReview
+)
 
 // 4) Update a user's review. PATCH /reviews/own
 router.patch(
@@ -25,9 +30,14 @@ router.patch(
   authenticate,
   validateBody(schemas.addReviewSchema),
   reviewController.updateReviewById
-);
+)
 
 // 5) Delete a user's review.
-router.delete('/:id', isValidId, authenticate, reviewController.deleteReviewById);
+router.delete(
+  '/:id',
+  isValidId,
+  authenticate,
+  reviewController.deleteReviewById
+)
 
-module.exports = router;
+module.exports = router
